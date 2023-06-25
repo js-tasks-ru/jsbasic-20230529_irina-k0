68d123a2d1ee142007777f891a5ccf53d59bcd8b
IrinaKond <94009167+IrinaKond@users.noreply.github.com>
2023-06-24 20:43:41 -0500

    Task 5.1 implementation

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,42 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.createTable();
+  }
+
+  createTable() {
+    if (!this.rows) {
+      return;
+    }
+    let theadTemplate = `
+    <tr>
+      <th>Имя</th>
+      <th>Возраст</th>
+      <th>Зарплата</th>
+      <th>Город</th>
+      <th></th>
+    </tr>`;
+
+    let table = document.createElement('table');
+    let thead = document.createElement('thead');
+    let tbody = document.createElement('tbody');
+
+    thead.innerHTML = theadTemplate;
+
+    for (let row of this.rows) {
+      let tr = document.createElement('tr');
+      tr.innerHTML = `
+        <td>${row.name}</td>
+        <td>${row.age}</td>
+        <td>${row.salary}</td>
+        <td>${row.city}</td>
+        <td><button onclick="(() => this.closest('tr').remove())()">X</button></td>`;
+        tbody.append(tr);
+    }
+    
+    table.append(thead);
+    table.append(tbody);
+    
+    this.elem = table;
   }
 }
